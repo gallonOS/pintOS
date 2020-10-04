c7622d655948b1d33df8da46727c8ffcba2f45dd
zjlin <zijunlin32@gmail.com>
2020-10-04 12:00:38 -1000

    started syscall_handler

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -18,3 +18,28 @@ syscall_handler (struct intr_frame *f UNUSED)
   printf ("system call!\n");
   thread_exit ();
 }
+
+
+syscall_handler(struct intr_frame *f){
+  //the saved stack
+  int *p = f->esp;
+  //if the stack pointer is invalid 
+  if(!valid(p))kill(); 
+  int syscall_number = *p;
+  switch(syscall_number)
+  {
+    case SYS_HALT:
+      halt();
+      break;
+    case SYS_EXIT:
+      if(!valid(p+1)) kill();
+      exit(*(p+1));
+      break
+    case SYS_EXEC:
+      if(!valid(p+1) || !valid(*(p+1))) kill();
+      
+    case SYS_WAIT:
+    case SYS_CREATE:
+    case
+  }
+}
