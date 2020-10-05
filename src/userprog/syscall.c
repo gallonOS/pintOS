cc377e0e9aeadf2c6475bf8ffa8f716835ecfa29
zjlin <zijunlin32@gmail.com>
2020-10-04 16:08:10 -1000

    feat - halt(), exit()

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -6,22 +6,26 @@
 
 static void syscall_handler (struct intr_frame *);
 
-void
-syscall_init (void) 
-{
+void syscall_init (void) {
   intr_register_int (0x30, 3, INTR_ON, syscall_handler, "syscall");
 }
+void halt(void){
+  shutdown_power_off();
+}
 
-static void
+void exit(int status){
+  //termintate current user program
+  struct thread *curr= thread_current();
+  if(active_thread(curr->parent)) cur->cp->status = status;
+  printf("%s: exit(%d)\n", curr->name,status);
+  thread_exit(); 
+}
+
 syscall_handler (struct intr_frame *f UNUSED) 
 {
   printf ("system call!\n");
   thread_exit ();
-}
-
-
-syscall_handler(struct intr_frame *f){
-  //the saved stack
+    //the saved stack
   int *p = f->esp;
   //if the stack pointer is invalid 
   if(!valid(p))kill(); 
@@ -37,9 +41,15 @@ syscall_handler(struct intr_frame *f){
       break
     case SYS_EXEC:
       if(!valid(p+1) || !valid(*(p+1))) kill();
-      
+
     case SYS_WAIT:
     case SYS_CREATE:
-    case
+    case 
   }
 }
+
+
+syscall_handler(struct intr_frame *f){
+
+}
+
